dc1c5eb122d712e36ebaac39a7c79f49f130ec9f
Dora Budić <dbudic0706@gmail.com>
2021-03-23 14:49:51 +0100

    Finished

---
+++ main.js
@@ -0,0 +1,9 @@
+var app = new Vue({
+  el: '#root',
+  data: {
+    message: 'Code is like humor. When you have to explain it, it’s bad.',
+    image: './img/vue.png',
+    imageDescription: 'vue logo',
+    colorTitle: 'green',
+  }
+});
